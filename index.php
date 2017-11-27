377af03519d1eade493b5cc1e44a5a862b0533a0
Matthew Cheung <mcheung@gmail.com>
2017-11-26 19:25:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
+<html>
+<head>
+<title>Crypto Portfolio</title>
+</head>
+<body>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script type="text/javascript">
-	var ticker = <?=file_get_contents('ticker');?>;
-	
+	var ticker;
 	function add_allocation (symbol, quantity, target)
 	{
 		var div = $('<div class="coin" />');
@@ -62,10 +66,19 @@
 		
 		var suggestion = $('<div data-field="suggestion" class="inline">0</div>');
 		suggestion.appendTo(div);
+
+		var btcprice = $('<div data-field="btc_price" class="inline">0</div>');
+		btcprice .appendTo(div);
+
+		var change = $('<div data-field="btc_change" class="inline">0</div>');
+		change.appendTo(div);
+
+		var btcvalue = $('<div data-field="btc_value" class="inline">0</div>');
+		btcvalue .appendTo(div);
 		
 		var price = $('<div data-field="price" class="inline">0</div>');
 		price.appendTo(div);
-		
+
 		var change = $('<div data-field="change" class="inline">0</div>');
 		change.appendTo(div);
 		
@@ -83,8 +96,7 @@
 		
 		var coin = get_from_ticker(symbol);
 		var link = $('<a/>').attr('href', link_to_coin(coin.id)).attr('target', '_blank').html(coin.name);
-		
-		div.children('div[data-field="name"]').append($(image(coin.id))).append(link);
+		div.children('div[data-field="name"]').html($(image(coin.id))).append(link);
 		
 		if (coin.symbol)
 			$(this).val(coin.symbol);
@@ -104,13 +116,18 @@
 	{
 		var input = $(this);
 		var div = input.parent();
-		var val = input.val();
+		var val = input.val().length == 0 ? '0' : input.val();
 		val = val.replace(/%/g, '');
 		input.val(val);
 		
 		setTimeout(function() { if (input.val().substr(-1) != '%' && ! input.is(":focus")) input.val(input.val()+'%'); }, 1000);
 		div.attr('data-target', parseInt(input.val()));
 		
+		refresh_portfolio();
+	}
+	
+	function refresh_portfolio ()
+	{
 		var target_total = 0;
 		$('div.coin').each(function ()
 		{
@@ -120,15 +137,12 @@
 		});
 		
 		if (target_total != 100)
-			$('#target_total').html('(sum='+target_total+'%)').addClass('negative');
+			$('#target_total').html('Target Total = '+target_total+'%').addClass('negative');
 		else
 			$('#target_total').html('').removeClass('negative');
-		
-		refresh_portfolio();
-	}
-	
-	function refresh_portfolio ()
-	{
+
+
+		var total_btc = 0;
 		var total_value = 0;
 		
 		$('div.coin').each(function ()
@@ -139,10 +153,15 @@
 				return;
 			
 			var coin = get_from_ticker(allocation.symbol);
+			var btc_value = allocation.quantity * coin.price_btc;
 			var value = allocation.quantity * coin.price_usd;
 			
+			total_btc += btc_value;
 			total_value += value;
 		});
+
+		var btc = get_from_ticker('BTC');
+		var btc_24h_price = btc.price_usd / ((100 + parseFloat(btc.percent_change_24h)) / 100);
 		
 		$('div.coin').each(function ()
 		{
@@ -152,21 +171,35 @@
 				return;
 			
 			var coin = get_from_ticker(allocation.symbol);
+			var btc_value = allocation.quantity * coin.price_btc;
 			var value = allocation.quantity * coin.price_usd;
 			var actual = 100 * value / total_value;
 			var suggestion = (allocation.target - actual) / 100 * total_value / coin.price_usd;
 			var drift = Math.abs(allocation.target - actual);
 			var drift_color = 'hsl(0, 100%, '+Math.round(drift>5?50:10*drift)+'%)';
+
+			var coin_24h_price = coin.price_usd / ((100 + parseFloat(coin.percent_change_24h)) / 100);
+			var coin_24h_btc_price = coin_24h_price / btc_24h_price;
+			var coin_percent_change_btc = (coin.price_usd / btc.price_usd - coin_24h_btc_price) / coin_24h_btc_price * 100;
+			coin_percent_change_btc = coin_percent_change_btc.toLocaleString('en-US', { maximumFractionDigits: 2, minimumFractionDigits: 1 });
 			
-			$(this).children('div[data-field="actual"]').html(actual.toFixed(2)+'%').css('color', drift_color);
-			$(this).children('div[data-field="suggestion"]').html((suggestion>0?'+':'') + suggestion.toFixed(2) + ' ' + coin.symbol);
-			$(this).children('div[data-field="price"]').html(coin.price_usd);
-			$(this).children('div[data-field="change"]').html((coin.percent_change_24h > 0 ? '+' : '') + coin.percent_change_24h + '%');
-			$(this).children('div[data-field="change"]').removeClass('positive').removeClass('negative').addClass(coin.percent_change_24h > 0 ? 'positive' : 'negative');
-			$(this).children('div[data-field="value"]').html(parseInt(value));
+			$(this).children('div[data-field="actual"]').html(actual.toLocaleString('en-US', { maximumFractionDigits: 1 })+'%').css('color', drift_color);
+			$(this).children('div[data-field="suggestion"]').html((suggestion>0?'+':'') + suggestion.toLocaleString('en-US', { maximumFractionDigits: 0 }));
+			$(this).children('div[data-field="btc_price"]').html(parseFloat(coin.price_btc).toFixed(8));
+			$(this).children('div[data-field="btc_change"]').html(coin_percent_change_btc + '%');
+			$(this).children('div[data-field="btc_value"]').html(parseFloat(btc_value).toFixed(4));
+			$(this).children('div[data-field="price"]').html(parseFloat(coin.price_usd).toLocaleString('en-US', { minimumFractionDigits: 2 }));
+			$(this).children('div[data-field="change"]').html(coin.percent_change_24h + '%');
+			$(this).children('div[data-field="value"]').html(parseInt(value).toLocaleString('en-US'));
+
+			$(this).children('div[data-field="btc_change"]').removeClass('positive').removeClass('negative');
+			$(this).children('div[data-field="change"]').removeClass('positive').removeClass('negative');
+			if (coin_percent_change_btc != 0) $(this).children('div[data-field="btc_change"]').addClass(coin_percent_change_btc > 0 ? 'positive' : 'negative');
+			if (coin.percent_change_24h != 0) $(this).children('div[data-field="change"]').addClass(coin.percent_change_24h > 0 ? 'positive' : 'negative');
 		});
 		
-		$('#grand_total span').html(total_value.toFixed(2));
+		$('#btc_total span').html(parseFloat(total_btc).toFixed(8));
+		$('#grand_total span').html(parseInt(total_value).toLocaleString('en-US'));
 		
 		store();
 	}
@@ -209,6 +242,13 @@
 				return;
 			allocations.push(allocation);
 		});
+
+		allocations.sort(function(a, b) {
+			var targetA = parseFloat(a.target), targetB = parseFloat(b.target);
+			var quantityA = parseFloat(a.quantity), quantityB = parseFloat(b.quantity);
+			if (targetA == targetB) return (quantityA < quantityB) ? 1 : (quantityA > quantityB) ? -1 : 0;
+			return (targetA < targetB) ? 1 : -1;
+		});
 		
 		localStorage.setItem('allocations', JSON.stringify(allocations));
 	}
@@ -231,12 +271,17 @@
 	{
 		var incomplete_allocation = undefined;
 		
-		if ($(div).children('input.hint').length)
+		if ($(div).find('input.hint').length)
 			return incomplete_allocation;
 		
 		var symbol = div.attr('data-symbol');
-		var quantity = div.attr('data-quantity');
-		var target = div.attr('data-target');
+
+		if (symbol.length == 0)
+			return incomplete_allocation;		
+
+		var quantity = div.attr('data-quantity').length == 0 ? '0' : div.attr('data-quantity');
+
+		var target = div.attr('data-target').length == 0 ? '0' : div.attr('data-target') ;
 		
 		return {
 			symbol: symbol,
@@ -248,8 +293,10 @@
 	function index_ticker ()
 	{
 		var new_ticker = {};
-		for (var i in ticker)
+		for (var i in ticker) {
+			if (ticker[i].symbol in new_ticker) continue;
 			new_ticker[ticker[i].symbol] = ticker[i];
+		}
 		ticker = new_ticker;
 	}
 	
@@ -260,14 +307,23 @@
 	
 	function link_to_coin (coin_id)
 	{
-		return 'http://www.google.com/search?q=site:coinmarketcap.com%20'+coin_id+'&btnI';
+		return 'http://coinmarketcap.com/currencies/'+coin_id;
 	}
 	
-	$(document).ready(function () {
+	function init()
+	{
 		index_ticker();
-		$('#add_allocation').click(add_allocation);
+		$('#add_allocation').click(function() { add_allocation() });
 		load();
 		add_allocation();
+	}
+
+	$(document).ready(function () {
+		$.get('https://api.coinmarketcap.com/v1/ticker/?limit=300', function(data) {
+			ticker = data;
+			init();
+		});
+
 	});
 </script>
 
@@ -276,24 +332,39 @@
 		font-family: Arial;
 		line-height: 1.3;
 	}
-	#header > div {
+	h2 {
+		margin-top: 5px;
+		margin-bottom: 25px;
+	}
+	a {
+		color: blue;
+		text-decoration: none;
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+	#header, #coins {
+		white-space: nowrap;
+		font-size: 0;
+	}
+	#header > div, .coin > * {
 		display: inline-block;
-		width: 150px;
+		width: 80px;
+		font-size: 12px;
+		margin: 5px;
+		text-align: right;
+	}
+	#header {
+		font-weight: bold;
 	}
 	input {
-		width: 150px;
-		margin-right: 5px;
+		padding: 2px;
 	}
 	input.hint {
 		color: #ccc;
-		font-style: italic;
 	}
 	input[data-field="target"]:after {
 		content: '%';
-	}
-	div.inline {
-		display: inline-block;
-		width: 150px;
 	}
 	div[data-field="price"]:before,
 	div[data-field="value"]:before {
@@ -303,22 +374,32 @@
 		cursor: pointer;
 		color: gray;
 	}
+
 	input[data-field="symbol"],
-	#header > div[data-field="symbol"] {
-		width: 70px;
-	}
 	input[data-field="target"],
-	#header > div[data-field="target"] {
-		width: 80px;
+	div[data-field="symbol"],
+	div[data-field="target"],
+	div[data-field="actual"]
+	{
+		width: 60px !important;
+	}
+	div[data-field="name"] {
+		font-size: 13px;
+		width: 160px !important;
+		text-align: left !important;
+	}
+	input[data-field="symbol"],
+	div[data-field="symbol"] {
+		text-align: center !important;
 	}
 	#grand_total span:before {
 		content: '$';
 	}
 	.positive {
-		color: green;
+		color: #093;
 	}
 	.negative {
-		color: red;
+		color: #d14836;
 	}
 	img {
 		position: relative;
@@ -328,24 +409,37 @@
 	#coins > div:nth-child(odd) {
 		background: #eee;
 	}
-	a {
-		text-decoration: none;
+	#coins > div:hover {
+		background: #fbf4c7;
 	}
 </style>
 
+<div style="float:right;">
+	<div id="btc_total"><b>Total BTC:</b> <span>0</span></div>
+	<div id="grand_total"><b>Total Value:</b> <span>0</span></div>
+</div>
+
+<h2><a href="http://coinmarketcap.com/" target="_blank">Crypto</a> Portfolio</h2>
+
+<div id="target_total"></div>
+
 <div id="header">
-	<div data-field="coin">Coin</div>
+	<div data-field="name">Name</div>
 	<div data-field="symbol">Symbol</div>
 	<div data-field="quantity">Quantity</div>
-	<div data-field="target">Target Alloc. <span id='target_total'></span></div>
-	<div data-field="actual">Actual Alloc.</div>
+	<div data-field="target">Target %</div>
+	<div data-field="actual">Actual %</div>
 	<div data-field="suggestion">Suggestion</div>
+	<div data-field="btc_price">BTC Price</div>
+	<div data-field="btc_change">BTC Change</div>
+	<div data-field="btc_value">BTC Value</div>
 	<div data-field="price">Price</div>
-	<div data-field="24h_change">24h Change</div>
-	<div data-field="value">Total Value</div>
+	<div data-field="change">$Change</div>
+	<div data-field="value">Value</div>
+	
 </div>
 <div id="coins"></div>
 
-<div id="add_allocation">[+]</div>
-
-<div id="grand_total">Grand Total: <span>0</span></div>
+<div id="add_allocation">[ + ]</div>
+</body>
+</html>
